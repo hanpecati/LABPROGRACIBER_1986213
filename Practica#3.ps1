8192467d541ff0290051ce932086c8c20d29eefb
hanpecati <70762162+hanpecati@users.noreply.github.com>
2020-10-17 03:51:50 -0500

    Add files via upload

---
+++ Practica#3.ps1
@@ -0,0 +1,25 @@
+﻿function Search-Tags
+{	
+	$etiqueta1 = Read-Host "Etiqueta de busqueda"
+	$etiqueta2 = Read-Host "Etiqueta de busqueda"
+	$etiqueta3 = Read-Host "Etiqueta de busqueda"
+	START http://www.news.google.com/search?q=$etiqueta1%20when%3A1d
+	START http://www.news.google.com/search?q=$etiqueta2%20when%3A1d
+	START http://www.news.google.com/search?q=$etiqueta3%20when%3A1d
+}
+function Search-News
+{
+	Search-Tags
+}
+$validate = Read-Host "Iniciar busqueda: Si[Y] No [N]?"
+
+if ($validate -eq "Y")
+{
+	Search-News
+	Write-Host "Searching..."
+	Write-Host "Busqueda iniciada con las etiquetas:" $etiqueta1, $etiqueta2, $etiqueta3
+}
+ElseIf ($validate -eq "N")
+{
+	Write-Host "Canceling search..."
+}
